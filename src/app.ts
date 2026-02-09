aea6e936ebe9d94a3f78c294b7f3fcc1b54e8cb2
ayathree <nobanitaayathree333@gmail.com>
2026-02-10 01:36:34 +0600

    Work in core project setup with prisma

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response } from "express";
+import { prisma } from "./app/lib/prisma";
+import { IndexRoutes } from "./app/route";
 
 const app: Application = express();
 
@@ -8,9 +10,20 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+app.use('/api/v1', IndexRoutes)
+
 // Basic route
-app.get('/', (req: Request, res: Response) => {
-    res.send('HealthySkin Server is running');
+app.get('/', async (req: Request, res: Response) => {
+    const specialty = await prisma.specialty.create({
+        data: {
+            title: "Acne Speacialist"
+        }
+    })
+    res.status(201).json({
+        success: true,
+        message: "Api is working",
+        data: specialty
+    })
 });
 
 export default app;
