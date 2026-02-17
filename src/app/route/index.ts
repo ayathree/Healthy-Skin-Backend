2048e33904c6d5390022df3e401867048bf4f1cf
ayathree <nobanitaayathree333@gmail.com>
2026-02-18 02:59:53 +0600

    create doctor,doctorSpeciality models and user module

--- src/app/route/index.ts
+++ src/app/route/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { specialtyRoutes } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { UserRoutes } from "../module/user/user.route";
 
 
 const router = Router();
 
 router.use("/specialties", specialtyRoutes)
 router.use("/auth", AuthRoutes)
+router.use("/doctors", UserRoutes)
 
 export const IndexRoutes = router
