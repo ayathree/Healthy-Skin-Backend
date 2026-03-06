8b29aeca4b1e69ce4bdea01a7b796c56d578ef7e
ayathree <nobanitaayathree333@gmail.com>
2026-03-06 23:03:07 +0600

    query building setup

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,9 +1,82 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary, UploadApiResponse } from 'cloudinary';
 import { envVars } from './env';
+import AppError from '../errorHelper/appError';
+import status from 'http-status';
+import { buffer } from 'node:stream/consumers';
+import { resolve } from 'node:dns';
+import { rejects } from 'node:assert';
+import { Result } from 'pg';
 
 cloudinary.config({
     cloud_name: envVars.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
     api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
     api_secret: envVars.CLOUDINARY.CLOUDINARY_API_SECRET
 })
+
+export const uploadFileCloudinary = async (
+    buffer: Buffer,
+    fileName: string
+): Promise<UploadApiResponse> => {
+    if (!buffer || !fileName) {
+        throw new AppError(status.BAD_REQUEST, "File buffer and file name are required for upload")
+    }
+
+    const extension = fileName.split(".").pop()?.toLocaleLowerCase();
+
+    const fileNameWithoutExtension = fileName
+        .split(".")
+        .slice(0, -1)
+        .join(".")
+        .toLocaleLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(/[^a-z0-9\-]/g, "");
+
+    const uniqueName =
+        Math.random().toString(36).substring(2) +
+        "-" +
+        Date.now() +
+        "-" +
+        fileNameWithoutExtension;
+
+    const folder = extension === "pdf" ? 'pdfs' : 'images';
+
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream(
+            {
+                resource_type: "auto",
+                public_id: `healthySkin/${folder}/${uniqueName}`,
+                folder: `healthySkin/${folder}`
+            },
+            (error, result) => {
+                if (error) {
+                    return reject(new AppError(status.INTERNAL_SERVER_ERROR, "Failed to upload file to cloudinary"))
+                }
+                resolve(result as UploadApiResponse)
+            }
+        )
+    })
+
+}
+
+export const deleteFileFromCloudinary = async (url: string) => {
+    try {
+        const regex = /\/v\d+\/(.+?)(?:\.[a-zA-Z0-9]+)+$/;
+        const match = url.match(regex)
+        if (match && match[1]) {
+            const publicId = match[1]
+
+            await cloudinary.uploader.destroy(
+                publicId, {
+                resource_type: "image"
+            }
+            )
+            console.log(`File ${publicId} deleted from cloudinary`)
+        }
+    } catch (error) {
+        console.error("Error deleting file from cloudinary:", error);
+        throw new AppError(status.INTERNAL_SERVER_ERROR, "Failed to delete file from cloudinary")
+    }
+}
+
+
 export const cloudinaryUpload = cloudinary
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -5,11 +5,20 @@ import z, { any, number } from "zod";
 import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
 import { handelZodError } from "../errorHelper/handelZodError";
 import AppError from "../errorHelper/appError";
+import { deleteFileFromCloudinary } from "../config/cloudinary.config";
 
 
-export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+export const globalErrorHandler = async (err: any, req: Request, res: Response, next: NextFunction) => {
     if (envVars.NODE_ENV === 'development') {
         console.log("Error from Global Error Handler", err)
+    }
+    if (req.file) {
+        await deleteFileFromCloudinary(req.file.path)
+
+    }
+    if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+        const imageUrls = req.files.map((file) => file.path);
+        await Promise.all(imageUrls.map(url => deleteFileFromCloudinary(url)))
     }
 
 
